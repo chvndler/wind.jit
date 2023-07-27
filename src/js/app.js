5b72afff5a5b9aa7fff0f175a93560675f32438d
chvndler <chvndler.ch@icloud.com>
2023-07-27 16:16:50 -0400

    upd

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,9 @@
+(function () {
+  console.log('Hello, from wind.jit');
+  console.log('%cHello, from wind.jit', 'color: darkseagreen;');
 
+  // Rest of the code...
+})();
 
 console.log('Hello, from wind.jit');
-
 console.log('%cHello, from wind.jit', 'color: darkseagreen;');
